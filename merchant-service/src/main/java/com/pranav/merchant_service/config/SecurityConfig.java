608ca86af014e9fc415029ecf4563ff53d28aea1
Pranav Deshmukh <pranavdeshmukh5454@gmail.com>
2026-06-18 12:28:44 +0530

    Merchant Services chnages

--- merchant-service/src/main/java/com/pranav/merchant_service/config/SecurityConfig.java
+++ merchant-service/src/main/java/com/pranav/merchant_service/config/SecurityConfig.java
@@ -8,8 +8,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -34,8 +32,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/v3/api-docs.yaml",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html",
-                                "/actuator/health"
+                                "/swagger-ui.html"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
